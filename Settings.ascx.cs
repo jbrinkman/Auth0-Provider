59ea1d50011500471723bd16b923089677e27ffb
Joe Brinkman <joe@brinkman.me>
2019-06-15 18:59:16 -0400

    Fix issue #1 - Missing settings.ascx.cs file

---
+++ Settings.ascx.cs
@@ -0,0 +1,14 @@
+#region Usings
+
+using Dnn.Authentication.Auth0.Components;
+using System;
+
+#endregion
+
+namespace Dnn.Authentication.Auth0
+{
+    public partial class Settings : Auth0SettingsBase
+    {
+
+    }
+}
